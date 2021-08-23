9d0d3560b78fa131a2253ec77475726829309b5a
GreenProd <89426594+GreenProd@users.noreply.github.com>
2021-08-24 02:15:40 +0300

    Update HOUSEclearfoliage.data.js

--- HOUSEclearfoliage.data.js
+++ HOUSEclearfoliage.data.js
@@ -18,8 +18,8 @@ Module.expectedDataFileDownloads++;
     } else {
       throw 'using preloaded data can only be done on a web page or in a web worker';
     }
-    var PACKAGE_NAME = 'D:/Unreal testdrive/Migrate/HOUSE/HOUSEclearfoliage/Binaries/HTML5/HOUSEclearfoliage.data';
-    var REMOTE_PACKAGE_BASE = 'HOUSEclearfoliage.data';
+    var PACKAGE_NAME = 'https://dl.dropboxusercontent.com/s/dhqtrn8r4qfocts/HOUSEclearfoliage.data?dl=0';
+    var REMOTE_PACKAGE_BASE = 'https://dl.dropboxusercontent.com/s/dhqtrn8r4qfocts/HOUSEclearfoliage.data?dl=0';
     if (typeof Module['locateFilePackage'] === 'function' && !Module['locateFile']) {
       Module['locateFile'] = Module['locateFilePackage'];
       err('warning: you defined Module.locateFilePackage, that has been renamed to Module.locateFile (using your locateFilePackage for now)');
